212e480a470da7328ed0b91fc263c83b02aef5a1
Brian Penner <briancpenner@hotmail.com>
2021-11-08 12:19:14 -0800

    further cors work on server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express")
+var cors = require('cors')
 var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
 var port = process.env.PORT || 5000
-var cors = require('cors');
 
+app.use(cors())
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(cors());
 
 var dbUrl = 'mongodb+srv://mca:Passw0rd2021@mongo-node.suans.mongodb.net/mongo-node?retryWrites=true&w=majority'
 
@@ -26,13 +26,13 @@ var messages = [
 ]
 **/
 
-app.get('/messages',(req,res) =>{
+app.get('/messages', cors(), (req,res) =>{
     Message.find({}, (err,messages) =>  {
         res.send(messages)
     })
 })
 
-app.post('/messages',(req,res) =>{
+app.post('/messages', cors(), (req,res) =>{
     var message = new Message(req.body)
     message.save((err) => {
         if (err) 
