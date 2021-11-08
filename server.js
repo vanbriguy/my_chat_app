d25f3ccf0d083ccf95e7a5b6c936f91ef9a712e0
Brian Penner <briancpenner@hotmail.com>
2021-11-08 10:55:40 -0800

    added res. sendSetatus(500) to app.post

--- server.js
+++ server.js
@@ -36,11 +36,10 @@ app.post('/messages',(req,res) =>{
     var message = new Message(req.body)
     message.save((err) => {
         if(err)
-            sendStatus(500)
-
-//    messages.push(req.body)
-    io.emit('message',req.body)
-    res.sendStatus(200)
+            res.sendStatus(500)
+//  messages.push(req.body)
+            io.emit('message',req.body)
+            res.sendStatus(200)
     })
 })
 
