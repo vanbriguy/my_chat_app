93158d6fdf586e6f7ae3ea05887dc5c381076874
brian penner <briancpenner@gmail.com>
2022-03-29 12:26:55 -0700

    major changes. added user db, changed styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,51 +1,82 @@
 /* global document,window,localStorage */
 
 var socket = io()
-$(() => {
-    $("#send").click(() => {
-        var message = {name: $('#yourName').text(), message: $("#message").val(), timestamp: new Date()}
-        postMessage(message)
-    })
-    getMessages()
-})
-socket.on('message',addMessage)
 
-function addMessage(message){
+//trigger click get, add, post messages
+    $(() => {
+        $("#send").click(() => {
+            var message = {name: $('#yourName').text(), message: $("#message").val(), timestamp: new Date()}
+            postMessage(message)
+        })
+        getMessages()
+    })
+    socket.on('message',addMessage)
+
+    function addMessage(message){
         $("#messages").append(`<h4> From: ${message.name} </h4> <p> Message: ${message.message}</p> <p> Time: ${message.timestamp} </p><hr>`)
-}
-
-function getMessages() {
-    $.get('/messages', (data) => {
-        data.forEach(addMessage)
-    })
-}
-
-function postMessage(message) {
-    $.post('/messages', message )
-    location.reload();
-}
-
-// user login code
-function setUserName() {
-    var myName = window.prompt('Please enter your name');
-    localStorage.setItem('name', myName);
-    location.reload();
- //   $("#personalGreeting").append(`: Welcome, ${myName}`);
     }
 
-$(() => {
-    $("#userName").click(() => {
-        setUserName()
+    function getMessages() {
+        $.get('/messages', (data) => {
+            data.forEach(addMessage)
         })
-})
+    }
 
-$(() => {
-    if (!localStorage.getItem('name')) {
-        setUserName();
-        } else {
-            var storedName = localStorage.getItem('name');
-            $("#personalGreeting").append(`: Welcome, ${storedName}`)
-            $("#yourName").append(`${storedName}`)
-            }
-})
+    function postMessage(message) {
+        $.post('/messages', message )
+        location.reload();
+    }
+
+
+//trigger add,get,post user
+
+    $(() => {
+        $("#send").click(() => {
+            var user = {name: $('#yourName').text(), timestamp: new Date()}
+            postUser(user)
+        })
+        getUsers()
+    })
+    socket.on('user',addUser)
+
+    
+    function addUser(user){
+        $("#yourName").append(`User: ${storedName} `)
+    }
+
+    function getUsers() {
+        $.get('/users', (data) => {
+            data.forEach(addUser)
+        })
+    }
+
+    function postUser(user) {
+        $.post('/users', user )
+        location.reload();
+    }
+    
+
+
+// user login
+    function setUserName() {
+        var myName = window.prompt('Please enter your name');
+        localStorage.setItem('name', myName);
+        location.reload();
+        }
+
+    $(() => {
+        $("#userName").click(() => {
+            setUserName()
+            })
+    })
+
+    $(() => {
+        if (!localStorage.getItem('name')) {
+            setUserName();
+            } else {
+                var storedName = localStorage.getItem('name');
+                $("#personalGreeting").append(`: Welcome, <b> ${storedName} </b>`)
+                $("#yourName").append(`${storedName}`)
+                }
+    })
 
--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 
-var express = require("express")
-var cors = require('cors')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
-var port = process.env.PORT || 5000
+var express = require("express");
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname))
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(express.static(__dirname));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
 
 var dbUrl = 'mongodb+srv://mca:Passw0rd2021@mongo-node.suans.mongodb.net/mongo-node?retryWrites=true&w=majority'
 
 //fixie:KMDtBYBwMO92Zng@speedway.usefixie.com:1080
 
+//Message model for mongodb
 var Message = mongoose.model('Message', {
     name: String,
     message: String,
     timestamp: String
 })
-/*
+
+
+//User model for mongodb
+
 var User = mongoose.model('User', {
     name: String,
-    password: String,
     timestamp: String
 })
-*/
+
+
 //get messages from mongodb
 app.get('/messages', cors(), (req,res) => {
     Message.find({}, (err,messages) => {
@@ -36,15 +42,19 @@ app.get('/messages', cors(), (req,res) => {
         console.log('messages received successfully')
     })
 })
-/*
+
+
 //get users from mongodb
+
 app.get('/users', cors(), (req,res) => {
     User.find({}, (err,users) => {
         res.send(users)
         console.log('users received successfully')
     })
 })
-*/
+
+
+//post messages to mongodb
 app.post('/messages', cors(), (req,res) =>{
     var message = new Message(req.body)
     message.save((err) => {
@@ -54,6 +64,21 @@ app.post('/messages', cors(), (req,res) =>{
                 io.emit('message',req.body)
                 res.sendStatus(200)
                 console.log('message posted successfully');
+                }
+    })
+})
+
+//post users to mongodb
+
+app.post('/users', cors(), (req,res) =>{
+    var user = new User(req.body)
+    user.save((err) => {
+        if (err) 
+            console.log(err) 
+            else {
+                io.emit('user',req.body)
+                res.sendStatus(200)
+                console.log('user posted successfully');
                 }
     })
 })
@@ -70,5 +95,4 @@ mongoose.connect(dbUrl, (err) => {
 var server = http.listen(port, () => {
     console.log("Server is listening on port %d", port )
 })
-
 
